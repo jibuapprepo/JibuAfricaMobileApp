a624b1d27ea800b9702aa535e1217abd05b20a7a
jibuapprepo <jibu@napvibe.com>
2025-08-20 00:24:59 +0300

    Add files via upload

---
+++ src/core/features/jibu/jibu.page.ts
@@ -0,0 +1,34 @@
+/**
+ * (C) Copyright 2025 Jibu Africa.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-jibu',
+    templateUrl: './jibu.page.html',
+    styleUrls: ['./jibu.page.scss'],
+})
+export class JibuPage implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+        // Lifecycle hook - runs when the page initializes
+    }
+
+}
